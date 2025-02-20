da878f61eb239051e2803667245fc745f04ad9e4
harshil.junakiya <harshiljunakiya123@gmail.com>
2025-02-20 16:02:18 +0530

    Completed HomePage Design

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+    return (
+        <div className='md:mx-10'>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+                <div>
+                    <img className='mb-5 w-40' src={assets.logo} alt="" />
+                    <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-2 text-gray-600'>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-2 text-gray-600'>
+                        <li>+1-212-456-7890</li>
+                        <li>harshiljunakiya123@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright 2025@ DocSync - All Right Reserved.</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
