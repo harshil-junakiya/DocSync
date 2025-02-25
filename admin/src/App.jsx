4459fe57bebe1253067bb7c4a8c1617b37088bf6
harshil.junakiya <harshiljunakiya123@gmail.com>
2025-02-25 09:35:21 +0530

    Admin Login Done

---
+++ admin/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from './context/adminContext';
+
+
+const App = () => {
+
+  const { aToken } = useContext(AdminContext)
+
+  return aToken ? (
+    <div>
+      <ToastContainer />
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
