99d64fb55fb13d6db5ae3dbe29ba4a9180810ca9
harshil.junakiya <harshiljunakiya123@gmail.com>
2025-02-25 23:44:21 +0530

    Login API deployed

--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -8,7 +8,7 @@ const SideBar = () => {
     const {aToken} = useContext(AdminContext)
 
   return (
-    <div className='min-h-screen bg-white border-r'>
+    <div className='min-h-screen bg-white border-r border-gray-100'>
     {
         aToken && <ul className='text-[#515151] mt-5 '>
             <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#f2f3ff] border-r-4 border-[#5f6fff]' : ''}`} to={'/admin-dashboard'}>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
+  const {backendUrl, token, setToken} = useContext(AppContext)
+  const navigate = useNavigate()
   const [state, setState] = useState('Sign Up')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -9,11 +15,40 @@ const Login = () => {
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
+  
+    try {
+      if (state === 'Sign Up') {
+        const response = await axios.post(backendUrl + '/api/user/register', { name, password, email });
+        if (response.data.success) {
+          localStorage.setItem('token', response.data.token);
+          setToken(response.data.token);
+        } else {
+          toast.error(response.data.message);
+        }
+      } else {
+        const response = await axios.post(backendUrl + '/api/user/login', { password, email });
+        console.log(response); 
+        if (response.data.success === true) {
+          localStorage.setItem('token', response.data.token);
+          setToken(response.data.token);
+          console.log("Token stored:", response.data.token);
+        } else {
+          toast.error(response.data.message);
+        }
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
-  }
+  useEffect(()=> {
+    if(token){
+      navigate('/')
+    }
+  },[token])
 
   return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
         <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
         <p>Please {state === 'Sign Up' ? "sign up" : "login"} to book appointment</p>
@@ -30,9 +65,9 @@ const Login = () => {
         </div>
         <div className='w-full'>
           <p>Password</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(e) => setPassword(e.target.value)} value={password} required />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
         </div>
-        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>
+        <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'>
           {state === 'Sign Up' ? "Create Account" : "Login"}
         </button>
         {
