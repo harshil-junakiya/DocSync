aa2f6574f4cf5fb6efff8f1881559fc5dfd33b87
harshil.junakiya <harshiljunakiya123@gmail.com>
2025-02-25 16:17:55 +0530

    Add Doctor Functionality completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,6 +2,13 @@ import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/adminContext';
+import Navbar from './components/Navbar';
+import SideBar from './components/SideBar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/admin/Dashboard';
+import AllAppointments from './pages/admin/AllAppointments';
+import AddDoctor from './pages/admin/AddDoctor';
+import DoctorsList from './pages/admin/DoctorsList';
 
 
 const App = () => {
@@ -9,8 +16,20 @@ const App = () => {
   const { aToken } = useContext(AdminContext)
 
   return aToken ? (
-    <div>
+    <div className='bg-[#f8f9fd]'>
       <ToastContainer />
+      <Navbar />
+      <div className='flex items-start'>
+        <SideBar /> 
+        <Routes>
+           <Route path='/' element={<></>} />
+           <Route path='/admin-dashboard' element={<Dashboard />} />
+           <Route path='/all-appointments' element={<AllAppointments />} />
+           <Route path='/add-doctor' element={<AddDoctor />} />
+           <Route path='/doctor-list' element={<DoctorsList />} />
+
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
