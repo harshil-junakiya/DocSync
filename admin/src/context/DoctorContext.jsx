0664c836fa2bd17f5bc6c83e56b81dd82d47d248
harshil.junakiya <harshiljunakiya123@gmail.com>
2025-02-27 14:13:36 +0530

    Completed project

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -11,6 +11,7 @@ const DoctorContextProvider = (props) => {
     const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const [appointments, setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
+    const [profileData, setProfileData] = useState(false)
 
     const getAppointments = async () => {
         try {
@@ -74,6 +75,22 @@ const DoctorContextProvider = (props) => {
         }
     }
 
+    const getProfileData = async () =>{
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/profile', {headers: {dToken}})
+            if(data.success){
+                setProfileData(data.profileData)
+                console.log(data.profileData);
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
+
     const value = {
         dToken, setDToken,
         backendUrl, 
@@ -81,7 +98,9 @@ const DoctorContextProvider = (props) => {
         getAppointments,
         completeAppointment, cancelAppointment,
         dashData, setDashData,
-        getDashData
+        getDashData,
+        profileData, setProfileData,
+        getProfileData
     }
 
     return (
