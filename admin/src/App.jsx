c31af515acfebe13e00b37ce80b16257b5f271db
Harshil Junakiya <96239602+harshil-junakiya@users.noreply.github.com>
2025-04-25 18:13:31 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
-import { AdminContext } from './context/adminContext';
+import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
 import SideBar from './components/SideBar';
 import { Route, Routes } from 'react-router-dom';
