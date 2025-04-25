7ad61830dbf4a841568483e31dbcc4212df582ea
Harshil Junakiya <96239602+harshil-junakiya@users.noreply.github.com>
2025-04-25 18:10:16 +0530

    Update main.jsx with AppContext

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AdminContextProvider from './context/adminContext.jsx'
 import DoctorContextProvider from './context/DoctorContext.jsx'
-
+// updated appcontext
 import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
